d90825288e538170366c083a817a73589907a776
Sumit.Pandey <sumit.pandey@geminisolutions.com>
2023-07-06 11:08:13 +0530

    Logging Applied

--- src/main/java/com/assignment/bankManagementSystem/controller/AccountController.java
+++ src/main/java/com/assignment/bankManagementSystem/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.assignment.bankManagementSystem.controller;
 import com.assignment.bankManagementSystem.entities.Accounts;
 
 import com.assignment.bankManagementSystem.services.AccountServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,74 +16,93 @@ import java.util.List;
 public class AccountController {
     @Autowired
     private AccountServices accountServices;
-
+    private Logger logger= LoggerFactory.getLogger(AccountController.class);
     @PostMapping("/openAccount")
     public ResponseEntity<Accounts> openAccount(@RequestBody Accounts account){
+        logger.info("Creating account: {}",account );
         Accounts createdAccount=accountServices.openAccount(account);
         return ResponseEntity.ok(createdAccount);
     }
     @GetMapping("/accounts/{accountNumber}")
     public ResponseEntity<Accounts> getAccountByAccountNumber(@PathVariable("accountNumber") String accountNumber){
+        logger.info("Fetching Account By Given Account number: {}",accountNumber );
         Accounts account =accountServices.getAccountByAccountNumber(accountNumber);
         if (account!=null){
+            logger.info("Account found");
             return ResponseEntity.ok(account);
         }
         else{
+            logger.warn("Account not found");
             return ResponseEntity.notFound().build();
         }
     }
     @GetMapping("allAccounts/user/{userId}")
     public ResponseEntity<List<Accounts>> getAccountsByUserId (@PathVariable("userId") int userId)
     {
+        logger.info("Fetching all accounts that are associated to a single user by userId: {}",userId );
         List<Accounts> accountDetails=accountServices.getAccountsByUserId(userId);
         if(!accountDetails.isEmpty())
-        {
+        {   logger.info("All accounts found");
             return ResponseEntity.ok(accountDetails);
         }
         else {
+            logger.warn("No account found");
             return ResponseEntity.notFound().build();
         }
     }
     @PostMapping("/deposit/{accountNumber}")
     public ResponseEntity<Accounts> deposit(@PathVariable String accountNumber,@RequestParam double depositAmount){
+
+        logger.info("Depositing amount {} into account number {}",depositAmount,accountNumber);
         Accounts account=accountServices.deposit(accountNumber,depositAmount);
         if (account!=null){
+            logger.info("Amount deposited");
             return ResponseEntity.ok(account);
         }
         else{
+            logger.warn("Account not found");
             return ResponseEntity.notFound().build();
         }
     }
     @PostMapping("/withdraw/{accountNumber}")
     public ResponseEntity<Accounts> withdraw(@PathVariable String accountNumber,@RequestParam double withdrawAmount){
+        logger.info("Withdrawing amount {} from account number {}",withdrawAmount,accountNumber);
         Accounts account=accountServices.getAccountByAccountNumber(accountNumber);
         if (account!=null&&withdrawAmount<=account.getBalance()){
             account=accountServices.withdraw(accountNumber,withdrawAmount);
+            logger.info("Amount withdraw success");
             return ResponseEntity.ok(account);
         }
         else{
+            logger.warn("Amount withdraw failed");
             return ResponseEntity.internalServerError().build();
         }
     }
 
     @GetMapping("/balance/{accountNumber}")
     public ResponseEntity<Double> balanceEnquiry(@PathVariable String accountNumber){
+        logger.info("Fetching balance of account number {}",accountNumber);
         Double balance=accountServices.balanceEnquiry(accountNumber);
         if (balance!=null){
+            logger.info("Balance {}",balance);
             return ResponseEntity.ok(balance);
         }
         else{
+            logger.warn("Account not found");
             return ResponseEntity.notFound().build();
         }
     }
     @DeleteMapping("/accounts/{accountNumber}")
     public ResponseEntity<HttpStatus> deleteAccount(String accountNumber){
+        logger.info("Deleting account with account number {}",accountNumber);
         Accounts account=accountServices.getAccountByAccountNumber(accountNumber);
         if (account!=null){
             accountServices.deleteAccount(accountNumber);
+            logger.info("Account deleted successfully");
             return new ResponseEntity<>(HttpStatus.OK);
         }
         else{
+            logger.warn("Account does not exist");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
         }
 
--- src/main/java/com/assignment/bankManagementSystem/controller/UserController.java
+++ src/main/java/com/assignment/bankManagementSystem/controller/UserController.java
@@ -9,58 +9,74 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserServices userServices;
+    private Logger logger= LoggerFactory.getLogger(UserController.class);
 
 
 
     @GetMapping("/users")
     public ResponseEntity<List<Users>> getUsers(){
-
+    logger.info("Fetching all users");
         List<Users> usersList= userServices.getUsers();
         if (usersList.size()<=0){
+            logger.info("No user exist");
             return ResponseEntity.noContent().build();
         }
-        else { return ResponseEntity.ok(usersList);
+        else {
+            logger.warn("All users found");
+            return ResponseEntity.ok(usersList);
 
         }
     }
 
     @GetMapping("/users/{userId}")
     public ResponseEntity<Users> getUser(@PathVariable int userId){
-
+        logger.info("Fetching user by userId {}",userId);
         Users user=userServices.getUserById(userId);
         if (user!=null){
+            logger.info("User found");
             return ResponseEntity.ok(user);
         }
         else {
+            logger.warn("User not found");
             return ResponseEntity.notFound().build();
         }
     }
     @PostMapping("/users")
     public ResponseEntity<Users> addUser(@RequestBody Users userToAdd){
+        logger.info("Adding User {}",userToAdd);
         Users user=userServices.addUser(userToAdd);
         return ResponseEntity.ok(user);
 
     }
     @PutMapping("/users/{userId}")
     public ResponseEntity<Users> putUser(@RequestParam int userId,@RequestBody Users detailsToUpdate)    {
+        logger.info("Updating details of user {}",userId);
         Users updatedUser =userServices.updateUserDetails(userId,detailsToUpdate);
         return ResponseEntity.ok(updatedUser);
 
     }
     @DeleteMapping("/users/{userId}")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable int userId){
+        logger.info("Deleting user br userId {}",userId);
        Users user = userServices.getUserById(userId);
        if (user!=null){
            userServices.deleteUser(userId);
+           logger.info("User deleted");
+
            return new ResponseEntity<>(HttpStatus.OK);
        }
        else{
+           logger.warn("User not found");
+
            return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
        }
     }
